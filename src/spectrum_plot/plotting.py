6e6bd951c2087cab805e2b4697ed772f8eadfd8f
Lior <israelilior@gmail.com>
2022-02-14 21:06:59 +0200

    adding raw data to plot

--- src/spectrum_plot/plotting.py
+++ src/spectrum_plot/plotting.py
@@ -27,12 +27,13 @@ class Plot:
         self.seconds = None
         self.n = None
         self.image_index = 0
-        self.max_xy_value = [0, 0]
+        self.max_fft_xy_value = [0, 0]
+        self.max_raw_xy_value = [0, 0]
 
         self.add_musical_notes = add_musical_notes
 
         self.no_axis = False
-        fig, self.ax = plt.subplots(1, 1, figsize=figsize)
+        fig, [self.ax_raw, self.ax_fft] = plt.subplots(2, 1, figsize=figsize)
         plt.ion()
         plt.show()
 
@@ -53,29 +54,47 @@ class Plot:
         if self.add_musical_notes:
             self._add_notes()
         self._plot_fft()
-
-    def _clear_image(self):
-        self.ax.clear()
-        self.ax.grid(True)
-        self.ax.set_xlabel('Freq Hz')
-        self.ax.set_ylabel('Sine Amplitude')
-        if self.no_axis:
-            self.ax.set(xticks=[], yticks=[])
-            self.ax.axis('off')
-
-    def _plot_fft(self):
-        if self.fft.index[-1] > self.max_xy_value[0]:
-            self.max_xy_value[0] = self.fft.index[-1]
-        if self.fft.values.max() > self.max_xy_value[1]:
-            self.max_xy_value[1] = self.fft.values.max()
-        self.ax.set_xlim(0, self.max_xy_value[0])
-        self.ax.set_ylim(0, self.max_xy_value[1])
-        self.ax.plot(self.fft.index, self.fft.values)
+        self._plot_raw()
         plt.draw()
         plt.pause(0.001)
         if self.save_path is not None:
             plt.savefig(f'{self.save_path}/fft_{self.image_index:0>5}.jpg')
             self.image_index += 1
+
+    def _clear_image(self):
+        self.ax_fft.clear()
+        self.ax_fft.grid(True)
+        self.ax_fft.set_xlabel('Freq Hz')
+        self.ax_fft.set_ylabel('Sine Amplitude')
+        if self.no_axis:
+            self.ax_fft.set(xticks=[], yticks=[])
+            self.ax_fft.axis('off')
+
+        self.ax_raw.clear()
+        self.ax_raw.grid(True)
+        self.ax_raw.set_xlabel('time seconds')
+        self.ax_raw.set_ylabel('value')
+
+    def _plot_raw(self):
+        if self.seconds > self.max_raw_xy_value[0]:
+            self.max_raw_xy_value[0] = self.seconds
+        if max(self.raw_data) > self.max_raw_xy_value[1]:
+            self.max_raw_xy_value[1] = max(self.raw_data)
+        self.ax_raw.set_xlim(0, self.max_raw_xy_value[0])
+        self.ax_raw.set_ylim(-self.max_raw_xy_value[1], self.max_raw_xy_value[1])
+        self.ax_raw.plot(np.linspace(0, self.seconds, self.n), self.raw_data)
+
+
+    def _plot_fft(self):
+        if self.fft.index[-1] > self.max_fft_xy_value[0]:
+            self.max_fft_xy_value[0] = self.fft.index[-1]
+        if self.fft.values.max() > self.max_fft_xy_value[1]:
+            self.max_fft_xy_value[1] = self.fft.values.max()
+        self.ax_fft.set_xlim(0, self.max_fft_xy_value[0])
+        self.ax_fft.set_ylim(0, self.max_fft_xy_value[1])
+        self.ax_fft.plot(self.fft.index, self.fft.values)
+
+
 
     def _add_notes(self):
         max_note_index = from_freq(self.nyquist)
@@ -85,6 +104,6 @@ class Plot:
             max_note_index = max_note_index.note_index
         for note_index in range(1, max_note_index):
             note = from_note_index(note_index)
-            self.ax.axvline(note.freq, color='red')
-            self.ax.text(note.freq, 0, note.note, color='red', verticalalignment='top')
+            self.ax_fft.axvline(note.freq, color='red')
+            self.ax_fft.text(note.freq, 0, note.note, color='red', verticalalignment='top')
 
